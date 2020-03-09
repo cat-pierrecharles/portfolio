d1a1919ac41d043a2f2d5eca0cf3a5e1007fa808
Pierre Charles <p.charles0501@gmail.com>
2020-03-09 23:08:49 +0000

    Finishing up about me section

--- src/javascript/components/About.js
+++ src/javascript/components/About.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import '../../stylesheets/About.scss'
+import profilePicture from '../../assets/profilePicture.jpg'
 
 const About = () => {
   return (
     <div className='container pt-5 px-5'>
-          <div className='mb-5'>
-            <h1 className='about'>A bit about me</h1>
-          </div>
-          <p>
-          Hello! I'm Pierre, a Computer Science student based in London, who enjoys building and designing apps. I develop exceptional websites and web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends. Before starting my third year at university, I spent a year working at Holiday Extras as a front end developer specialising in JavaScript and React where I worked on a wide variety of interesting and meaningful projects on a daily basis. Here are a few technologies I've been working with recently:
-          </p>
+      <h1 className='about mb-5'>A bit about me</h1>
+      <div className='row'>
+        <div className='col-md-7'>
+          <p>Hello there! I'm Pierre, a third year Computer Science student based in Canterbury, Kent. I enjoy building and designing apps with a strong focus in UX and UI.</p>
+          <p>Before starting my third year at university, I spent a year working at Holiday Extras as a front end developer specialising in JavaScript and React where I worked on a wide variety of interesting projects.</p>
           <ul className='pt-4'>
-            <li>JavaScript (ES6+)</li>
+            <li>JavaScript</li>
             <li>React</li>
             <li>NodeJS</li>
             <li>HTML</li>
-            <li>SCSS</li>
+            <li>CSS</li>
             <li>Express</li>
           </ul>
         </div>
-  )
-}
-
-export default About
+        <div className='col-md-5'>
+          <img src={profilePicture}></img>
+        </div>
+      </div>
+    </div>
+    )
+  }
+  
+  export default About
