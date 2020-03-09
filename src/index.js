8cd83aea75fdb3da7ba56e0ac40375dec69dc5a3
Pierre Charles <p.charles0501@gmail.com>
2020-03-09 10:58:08 +0000

    Adding typerJS to home page

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ if (typeof console === 'object') {
   console.log(`
   %cHello there! Thank you for visiting!
   If you\'re interested in the code that was used
-  then why not checkout my GitHub repository: https://github.com/Pierre-Charles/pierre-dev
+  then why not checkout my GitHub repository: https://github.com/Pierre-Charles/portfolio
              _
      _      (o>
     (o>    //|     In the meantime, enjoy these penguins!
     (/)_   V_/_ 
-  `,"color: #FFE01B; font-size: 12px; font-family: ''Open Sans', Helvetica, sans-serif;'")
+  `,"color: #b8a0ff; font-size: 12px; font-family: ''Open Sans', Helvetica, sans-serif;'")
   }
   
   ReactDOM.render(<App />, document.getElementById('root'))
--- src/javascript/components/Home.js
+++ src/javascript/components/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <div className='container'>
       <h1 className='intro mb-4'>Hi, my name is</h1>
-      <h2 className='title'>Pierre Charles.</h2>
-      <h3 className='short-bio mr-5'>I build creative applications for the web.</h3>
+      <h2 className='title'>Pierre Charles</h2>
+      <h3 className='short-bio mr-5'>I build <span className='typer' id='main' data-words=' creative, innovative, stylish' data-delay='150' data-delay='150'></span> applications for the web</h3>
       <div>
         <p className='description mt-5'>
           I am a front-end developer from London currently working as an IT Consultant at the Kent IT Consultancy. Outside of work, I am finishing my final year in Computer Science at the University of Kent in Canterbury.
