1f1edc4f52e567c691013016f4193910ba929abd
Pierre Charles <p.charles0501@gmail.com>
2020-03-09 21:45:10 +0000

    Fixing alignment issues

--- src/javascript/components/Home.js
+++ src/javascript/components/Home.js
@@ -3,10 +3,9 @@ import '../../stylesheets/Home.scss'
 
 const Home = () => {
   return (
-    <div className='container pt-5'>
-      <h1 className='intro mb-4'>Hi, my name is</h1>
-      <h2 className='title'>Pierre Charles</h2>
-      <h3 className='short-bio mr-5'>I build <span className='typer' id='main' data-words=' creative, innovative, stylish' data-delay='150' data-delay='150'></span> applications for the web</h3>
+    <div className='container pt-5 px-5'>
+      <h1 className='title'>Pierre Charles</h1>
+      <h2 className='short-bio mr-5'>I build <span className='typer' id='main' data-words=' creative, innovative, stylish' data-delay='150' data-delay='150'></span> applications for the web</h2>
       <div>
         <p className='description mt-5'>
           I am a front-end developer from London currently working as an IT Consultant at the Kent IT Consultancy. Outside of work, I am finishing my final year in Computer Science at the University of Kent in Canterbury.
