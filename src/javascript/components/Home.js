c44154c6fff9f3ef6d6dec7bb01aeec2f5cfe25b
Pierre Charles <p.charles0501@gmail.com>
2020-03-10 10:07:09 +0000

    Fixing typer span and opacity for particles

--- src/javascript/components/Home.js
+++ src/javascript/components/Home.js
@@ -5,9 +5,9 @@ const Home = () => {
   return (
     <div className='container pt-5 px-5'>
       <div className='row'>
-        <div className='col-sm-12 col-md-8'>
+        <div className=''>
           <h1 className='title'>Pierre Charles</h1>
-          <h2 className='short-bio mr-5'>I build <span className='typer changing-span' id='main' data-words=' creative, innovative, stylish' data-delay='150'></span> applications for the web</h2>
+          <h2 className='short-bio mr-5'>I build <span className='typer changing-span' id='main' data-words=' creative, innovative, stylish' data-delay='150'></span> <br/>applications for the web</h2>
           <div>
             <p className='description mt-5'>
               I am a front-end developer from London currently working as an IT Consultant at the Kent IT Consultancy. Outside of work, I am finishing my final year in Computer Science at the University of Kent in Canterbury.
