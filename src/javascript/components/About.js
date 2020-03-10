7a2e8a1e6137a7cf37bd5a4b034fb32642e60d06
Pierre Charles <p.charles0501@gmail.com>
2020-03-10 01:40:57 +0000

    Fixing mobile responsive layout

--- src/javascript/components/About.js
+++ src/javascript/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../../stylesheets/About.scss'
-import profilePicture from '../../assets/profilePicture.jpg'
+import profilePicture from '../../assets/img.jpg'
 
 const About = () => {
   return (
@@ -10,21 +10,28 @@ const About = () => {
         <div className='col-md-7'>
           <p>Hello there! I'm Pierre, a third year Computer Science student based in Canterbury, Kent. I enjoy building and designing apps with a strong focus in UX and UI.</p>
           <p>Before starting my third year at university, I spent a year working at Holiday Extras as a front end developer specialising in JavaScript and React where I worked on a wide variety of interesting projects.</p>
-          <ul className='pt-4'>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>NodeJS</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Express</li>
+          <p>Here are a few of the technical skills I gained when working on projects:</p>
+          <ul className='pt-4 fa-ul'>
+            <li><span class='fa-li'><i class='fas fa-egg'></i></span>JavaScript</li>
+            <li><span class='fa-li'><i class='fas fa-egg'></i></span>React</li>
+            <li><span class='fa-li'><i class='fas fa-egg'></i></span>Express</li>
+            <li><span class='fa-li'><i class='fas fa-egg'></i></span>NodeJS</li>
+            <li><span class='fa-li'><i class='fas fa-egg'></i></span>HTML</li>
+            <li><span class='fa-li'><i class='fas fa-egg'></i></span>CSS</li>
+            <li><span class='fa-li'><i class='fas fa-egg'></i></span>Git</li>
+            <li><span class='fa-li'><i class='fas fa-egg'></i></span>UNIX</li>
           </ul>
         </div>
         <div className='col-md-5'>
-          <img src={profilePicture}></img>
+          <div className='py-5 img-bg'>
+            <div className='image ml-3'>
+              <img src={profilePicture}></img>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    )
-  }
-  
-  export default About
+  )
+}
+
+export default About
--- src/javascript/components/Home.js
+++ src/javascript/components/Home.js
@@ -4,12 +4,16 @@ import '../../stylesheets/Home.scss'
 const Home = () => {
   return (
     <div className='container pt-5 px-5'>
-      <h1 className='title'>Pierre Charles</h1>
-      <h2 className='short-bio mr-5'>I build <span className='typer' id='main' data-words=' creative, innovative, stylish' data-delay='150' data-delay='150'></span> applications for the web</h2>
-      <div>
-        <p className='description mt-5'>
-          I am a front-end developer from London currently working as an IT Consultant at the Kent IT Consultancy. Outside of work, I am finishing my final year in Computer Science at the University of Kent in Canterbury.
+      <div className='row'>
+        <div className='col-sm-12 col-md-8'>
+          <h1 className='title'>Pierre Charles</h1>
+          <h2 className='short-bio mr-5'>I build <span className='typer changing-span' id='main' data-words=' creative, innovative, stylish' data-delay='150'></span> applications for the web</h2>
+          <div>
+            <p className='description mt-5'>
+              I am a front-end developer from London currently working as an IT Consultant at the Kent IT Consultancy. Outside of work, I am finishing my final year in Computer Science at the University of Kent in Canterbury.
         </p>
+          </div>
+        </div>
       </div>
     </div>
   )
