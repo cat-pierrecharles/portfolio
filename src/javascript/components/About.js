a2ab3153611da54a03635fe5def364961c7dda07
Pierre Charles <p.charles0501@gmail.com>
2020-03-10 12:37:11 +0000

    Adding anchor for CV

--- src/javascript/components/About.js
+++ src/javascript/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
     <div className='container pt-5 px-5'>
       <h1 className='about mb-5'>A bit about me</h1>
       <div className='row'>
-        <div className='col-md-7'>
+        <div className='col-lg-7 col-md-6 col-sm-12'>
           <p>Hello there! I'm Pierre, a third year Computer Science student based in Canterbury, Kent. I enjoy building and designing apps with a strong focus in UX and UI.</p>
           <p>Before starting my third year at university, I spent a year working at Holiday Extras as a front end developer specialising in JavaScript and React where I worked on a wide variety of interesting projects.</p>
           <p>Here are a few of the technical skills I gained when working on projects:</p>
@@ -23,7 +23,7 @@ const About = () => {
             <li><span className='fa-li'><i className='fa fa-egg'></i></span>UNIX</li>
           </ul>
         </div>
-        <div className='col-md-5'>
+        <div className='col-lg-5 col-md-6 col-sm-6'>
           <div className='py-5 img-bg'>
             <Tilt className='Tilt ml-3' options={{ max: '25', transition: true, scale: 1 }}>
             <div className='Tilt-inner'>
--- src/javascript/components/Home.js
+++ src/javascript/components/Home.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import '../../stylesheets/Home.scss'
+import cv from '../../assets/cv.pdf'
 
 const Home = () => {
   return (
     <div className='container pt-5 px-5'>
-      <div className='row'>
-        <div className=''>
-          <h1 className='title'>Pierre Charles</h1>
-          <h2 className='short-bio mr-5'>I build <span className='typer changing-span' id='main' data-words=' creative, innovative, stylish' data-delay='150'></span> <br/>applications for the web</h2>
-          <div>
-            <p className='description mt-5'>
-              I am a front-end developer from London currently working as an IT Consultant at the Kent IT Consultancy. Outside of work, I am finishing my final year in Computer Science at the University of Kent in Canterbury.
-        </p>
-          </div>
+      <div className='main'>
+        <h1 className='title'>Pierre Charles</h1>
+        <h2 className='short-bio mr-5'>I build <span className='typer changing-span' id='main' data-words=' creative, innovative, stylish' data-delay='150'></span> <br />applications for the web</h2>
+        <div>
+          <p className='description mt-5'>
+            I am a front-end developer from London currently working as an IT Consultant at the Kent IT Consultancy. Outside of work, I am finishing my final year in Computer Science at the University of Kent in Canterbury.
+            </p>
         </div>
+          <a href={cv}>Download my CV</a>
       </div>
     </div>
   )
