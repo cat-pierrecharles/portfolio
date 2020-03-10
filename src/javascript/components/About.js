dffd167e8650c2cb3ffedbcae6214e7a83a2bc4f
Pierre Charles <p.charles0501@gmail.com>
2020-03-10 10:15:24 +0000

    Adding css animation for bullet points

--- src/javascript/components/About.js
+++ src/javascript/components/About.js
@@ -12,14 +12,14 @@ const About = () => {
           <p>Before starting my third year at university, I spent a year working at Holiday Extras as a front end developer specialising in JavaScript and React where I worked on a wide variety of interesting projects.</p>
           <p>Here are a few of the technical skills I gained when working on projects:</p>
           <ul className='pt-4 fa-ul'>
-            <li><span className='fa-li'><i className='fas fa-egg'></i></span>JavaScript</li>
-            <li><span className='fa-li'><i className='fas fa-egg'></i></span>React</li>
-            <li><span className='fa-li'><i className='fas fa-egg'></i></span>Express</li>
-            <li><span className='fa-li'><i className='fas fa-egg'></i></span>NodeJS</li>
-            <li><span className='fa-li'><i className='fas fa-egg'></i></span>HTML</li>
-            <li><span className='fa-li'><i className='fas fa-egg'></i></span>CSS</li>
-            <li><span className='fa-li'><i className='fas fa-egg'></i></span>Git</li>
-            <li><span className='fa-li'><i className='fas fa-egg'></i></span>UNIX</li>
+            <li><span className='fa-li'><i className='fa fa-egg'></i></span>JavaScript</li>
+            <li><span className='fa-li'><i className='fa fa-egg'></i></span>React</li>
+            <li><span className='fa-li'><i className='fa fa-egg'></i></span>Express</li>
+            <li><span className='fa-li'><i className='fa fa-egg'></i></span>NodeJS</li>
+            <li><span className='fa-li'><i className='fa fa-egg'></i></span>HTML</li>
+            <li><span className='fa-li'><i className='fa fa-egg'></i></span>CSS</li>
+            <li><span className='fa-li'><i className='fa fa-egg'></i></span>Git</li>
+            <li><span className='fa-li'><i className='fa fa-egg'></i></span>UNIX</li>
           </ul>
         </div>
         <div className='col-md-5'>
