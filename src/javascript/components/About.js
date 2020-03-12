62549128e9d4ab60a9d2d2a70796c5342629301f
Pierre Charles <p.charles0501@gmail.com>
2020-03-12 10:29:14 +0000

    Fixing fontawesome mobile rendering issue

--- src/javascript/components/About.js
+++ src/javascript/components/About.js
@@ -13,14 +13,14 @@ const About = () => {
           <p>Before starting my third year at university, I spent a year working at <a href='https://www.holidayextras.co.uk/'>Holiday Extras</a> as a Front End Developer specialising in JavaScript and React where I worked on a wide variety of interesting projects.</p>
           <p>Here are a few of the technical skills I gained when working on projects:</p>
           <ul className='pt-4 fa-ul'>
-            <li><span className='fa-li'><i className='fa fa-egg'></i></span>JavaScript</li>
-            <li><span className='fa-li'><i className='fa fa-egg'></i></span>React</li>
-            <li><span className='fa-li'><i className='fa fa-egg'></i></span>Express</li>
-            <li><span className='fa-li'><i className='fa fa-egg'></i></span>NodeJS</li>
-            <li><span className='fa-li'><i className='fa fa-egg'></i></span>HTML</li>
-            <li><span className='fa-li'><i className='fa fa-egg'></i></span>CSS</li>
-            <li><span className='fa-li'><i className='fa fa-egg'></i></span>Git</li>
-            <li><span className='fa-li'><i className='fa fa-egg'></i></span>UNIX</li>
+            <li><span className='fa-li'><i className='fas fa-egg'></i></span>JavaScript</li>
+            <li><span className='fa-li'><i className='fas fa-egg'></i></span>React</li>
+            <li><span className='fa-li'><i className='fas fa-egg'></i></span>Express</li>
+            <li><span className='fa-li'><i className='fas fa-egg'></i></span>NodeJS</li>
+            <li><span className='fa-li'><i className='fas fa-egg'></i></span>HTML</li>
+            <li><span className='fa-li'><i className='fas fa-egg'></i></span>CSS</li>
+            <li><span className='fa-li'><i className='fas fa-egg'></i></span>Git</li>
+            <li><span className='fa-li'><i className='fas fa-egg'></i></span>UNIX</li>
           </ul>
         </div>
         <div className='col-lg-5 col-md-6 col-sm-12'>
