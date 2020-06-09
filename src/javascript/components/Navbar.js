0fd71a29a618be45f62d78c2c1ce3e4ed317dbdf
Pierre Charles <p.charles0501@gmail.com>
2020-06-09 17:23:54 +0100

    Adding responsive navbar

--- src/javascript/components/Navbar.js
+++ src/javascript/components/Navbar.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-scroll'
 import '../../stylesheets/Navbar.scss'
 
 export default function Navbar() {
+  const [navActive, setNavActive] = useState(false)
   return (
-    <div className='fluid-container menu'>
+    <div className='fluid-container menu py-1'>
       <nav>
-        <div className='logo'>
+        <div className='logo ml-5'>
           <code className='color-tertiary'>&lt;pierreCharles &#47;&gt;</code>
         </div>
-        <ul className='nav-links p-3 m-3'>
-          <Link activeClass='active' to='home' spy={true} smooth={true} offset={0} duration={500}>Home</Link>
-          <Link activeClass='active' to='about' spy={true} smooth={true} offset={0} duration={500}>About</Link>
-          <Link activeClass='active' to='experience' spy={true} smooth={true} offset={0} duration={500}>Experience</Link>
-          <Link activeClass='active' to='projects' spy={true} smooth={true} offset={0} duration={500}>Projects</Link>
+        <div className='spacer' />
+        <ul className={navActive ? 'nav-links nav-active' : 'nav-links p-3 m-3 mr-3'}>
+          <Link onClick={() => { navActive ? setNavActive(!navActive) : null }} activeClass='active' to='home' spy={true} smooth={true} offset={0} duration={500}>Home</Link>
+          <Link onClick={() => { navActive ? setNavActive(!navActive) : null }} activeClass='active' to='about' spy={true} smooth={true} offset={0} duration={500}>About</Link>
+          <Link onClick={() => { navActive ? setNavActive(!navActive) : null }} activeClass='active' to='experience' spy={true} smooth={true} offset={0} duration={500}>Experience</Link>
+          <Link onClick={() => { navActive ? setNavActive(!navActive) : null }} activeClass='active' to='projects' spy={true} smooth={true} offset={0} duration={500}>Projects</Link>
+          <Link onClick={() => { navActive ? setNavActive(!navActive) : null }} activeClass='active' to='projects' spy={true} smooth={true} offset={0} duration={500}>Contact</Link>
         </ul>
+        <div className={navActive ? 'mobile-menu toggle mr-3' : 'mobile-menu mr-3'} onClick={() => { setNavActive(!navActive) }}>
+          <div className='top-bar'></div>
+          <div className='middle-bar'></div>
+          <div className='bottom-bar'></div>
+        </div>
       </nav>
     </div>
   )
